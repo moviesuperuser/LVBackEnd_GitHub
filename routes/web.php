bb8e7fb07a0a5490365efb640d1e925ad602019d
annguyenbku1999 <78542632+annguyenbku1999@users.noreply.github.com>
2021-05-03 19:31:35 +0700

    Update Upload Movie

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+use Illuminate\Support\Facades\DB;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,37 +16,49 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
 });
 
 Route::post('/upload', function (Request $request) {
-    
-  $filename = $request->file("thing")->store("1pc05ZmozeUN-ofSgO8ugg9wG7cQU2BL1","google") ;
+  $filename = $request->file("thing")->store("1pc05ZmozeUN-ofSgO8ugg9wG7cQU2BL1", "google");
   dump($filename);
   $googleDriveStorage = Storage::disk('google');
   $dir = '/';
   $recursive = true; // Get subdirectories also?
   $file = collect(Storage::disk('google')->listContents($dir, $recursive))
-      ->where('type', '=', 'file')
-      ->where('filename', '=', pathinfo($filename, PATHINFO_FILENAME))
-      ->where('extension', '=', pathinfo($filename, PATHINFO_EXTENSION))
-      ->sortBy('timestamp')
-      ->last();
-  dump("FILE NAME : ");
-  dump('_____________________________');
-  dump(Storage::Disk('google')->url($file['path']));
-  dump($file);
+    ->where('type', '=', 'file')
+    ->where('filename', '=', pathinfo($filename, PATHINFO_FILENAME))
+    ->where('extension', '=', pathinfo($filename, PATHINFO_EXTENSION))
+    ->sortBy('timestamp')
+    ->last();
+  // dump("FILE NAME : ");
+  // dump('_____________________________');
+  // dump(Storage::Disk('google')->url($file['path']));
+  // dump($file);
   #echo '<img src='.$link .' alt="Girl in a jacket">';
   $file_id = $file['basename'];
-  $link = 'https://drive.google.com/file/d/'.$file_id .'/preview';
-  dump($link);
-  #echo '<iframe frameborder="0" width="640" height="480" src="'.$link1.'" ; allowfullscreen="true" allow="autoplay"></iframe>';
-  echo '<iframe frameborder="0" width="640" height="480" src="'.$link.'" ; allowfullscreen="true" allow="autoplay"></iframe>';
+  if (isset($file_id)) {
+    $link = 'https://drive.google.com/file/d/' . $file_id . '/preview';
+    $mytime = Carbon\Carbon::now();
+    $action_check = DB::table('VideoLinks')
+          ->where('IdUser', $request['IdUser'])
+          ->insert([
+            'MovieName' => $request['name'],
+            'Link' => $link,
+            'Note' =>  $request['note'],
+            'created_at' => $mytime->toDateTimeString(),
+            'updated_at' => $mytime->toDateTimeString()
+          ]);
 
+    return redirect()->back()->withSuccess($link);
+  } else {
+    return redirect()->back()->withErrors('msg','Upload lỗi! Vui lòng thử lại.');
+  }
+  #echo '<iframe frameborder="0" width="640" height="480" src="'.$link1.'" ; allowfullscreen="true" allow="autoplay"></iframe>';
 });
 
 
-Route::get('newest', function() {
+Route::get('newest', function () {
   $filename = 'test.txt';
 
   Storage::disk('google')->put($filename, \Carbon\Carbon::now()->toDateTimeString());
@@ -52,10 +66,10 @@ Route::get('newest', function() {
   $dir = '/';
   $recursive = false; // Get subdirectories also?
   $file = collect(Storage::disk('google')->listContents($dir, $recursive))
-      ->where('type', '=', 'file')
-      ->where('filename', '=', pathinfo($filename, PATHINFO_FILENAME))
-      ->where('extension', '=', pathinfo($filename, PATHINFO_EXTENSION))
-      ->sortBy('timestamp')
-      ->last();
-      dump(Storage::Disk('google')->url($file['path']));
+    ->where('type', '=', 'file')
+    ->where('filename', '=', pathinfo($filename, PATHINFO_FILENAME))
+    ->where('extension', '=', pathinfo($filename, PATHINFO_EXTENSION))
+    ->sortBy('timestamp')
+    ->last();
+  dump(Storage::Disk('google')->url($file['path']));
 });
